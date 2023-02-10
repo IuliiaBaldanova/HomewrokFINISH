c29e36ba1fd00b6cf5c0da8c2783cdad3da53466
IuliiaBaldanova <yuliyatowa@mail.ru>
2023-02-10 12:50:12 -0500

    fix

---
+++ script.js
@@ -0,0 +1,22 @@
+const LOGIN=`My name is Iullia Baldanoba`;
+console.log(LOGIN)
+const WORK=`I am currently studying to be an IT specialist`;
+console.log(WORK)
+let thisYear= 2023;
+console.log(thisYear);
+let yearOfBirth=1990;
+console.log(yearOfBirth);
+let myAge= thisYear - yearOfBirth;
+console.log(myAge);
+let straboutMe= `I have wonderful children Tatiana and Maxim. 
+My daughter wants to become a surgeon. 
+Maxim dreams of becomingan engineer,as his idol is Ilon Mask. 
+My husband is an electrician. 
+I have always worked in government agencies. 
+My parents are pensioners`
+console.log(straboutMe);
+
+
+
+
+
