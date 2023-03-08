b0914003b9b322056473a3387ed3afb88d3b03b7
IuliiaBaldanova <yuliyatowa@mail.ru>
2023-03-08 11:56:55 -0500

    fix08-2

--- script.js
+++ script.js
@@ -1,100 +1,106 @@
-for (let i = 2; i <= 10; i++) {
-	if (i % 2 == 0) {
-        console.log(i) 
-    }
-   }
+      //   function check(numOne, numTwo) {
+      //   if (numOne < numTwo) {
+      //     return numOne;
+      //   } else {
+      //     return numTwo;
+      //   }
+      // }
+      
+      // let result = check(3,9);
+      // alert(result);
+      // let result1 = check(6,10);
+      // alert(result1);
 
-   
-for (let i = 0; ; i++) {
-    if (i === 5) {
-        break;
-    };
-}
+
+function raznica(priceCooking, priceSale) {
+  return priceSale - priceCooking;
+
+};
+
 
 let seafood = 
-    {
-    name : `carbonara`,
-    ingredients: [`noodles`,`bacon`,`cream`],
-    priceCooking: 0,
-    priceSale: 90,
-    
+{
+name : `carbonara`,
+ingredients: [`noodles`,`bacon`,`cream`],
+priceCooking: 0,
+priceSale: 90,
 };
 
 let japanese =     
-    {
-    name :`sushi`,
-    ingredients: [`rice`,`shrimps`,`avocado`],
-    priceCooking: 0,
-    priceSale: 105,
+{
+name :`sushi`,
+ingredients: [`rice`,`shrimps`,`avocado`],
+priceCooking: 0,
+priceSale: 105,
 };
 
 let buryad =     
-    {
-    name : `buuzi`,
-    ingredients: [`daugh`,`beef`,`pork`],
-    priceCooking: 0,
-    priceSale: 180,
+{
+name : `buuzi`,
+ingredients: [`daugh`,`beef`,`pork`],
+priceCooking: 0,
+priceSale: 180,
 };
 
 let dessert = 
-
-    {
-    name : `merenga`,
-    ingredients: [`protein`,`sugar`,`berries`],
-    priceCooking: 0,
-    priceSale:170,
+{
+name : `merenga`,
+ingredients: [`protein`,`sugar`,`berries`],
+priceCooking: 0,
+priceSale:170,
 };
 
 
 // massiv iz blud
 
 let menu = [
-    seafood,
-    japanese,
-    buryad,
-    dessert,
+seafood,
+japanese,
+buryad,
+dessert,
 ];
 
 //price ingredientov
 
 let price = {
-    noodles : 15,
-    bacon : 30,
-    cream : 25,
-    rice : 15, 
-    shrimps : 25,
-    avocado : 8,
-    daugh : 5,
-    beef : 23,
-    pork: 7,
-    protein : 12,
-    sugar : 15,
-    berries : 17,
+noodles : 15,
+bacon : 30,
+cream : 25,
+rice : 15, 
+shrimps : 25,
+avocado : 8,
+daugh : 5,
+beef : 23,
+pork: 7,
+protein : 12,
+sugar : 15,
+berries : 17,
 };
+
+//Посчитайте стоимость приготовления динамически (с помощью цикла)
 
 for (let i = 0; i < menu.length; i++) {
-    let sum = 0;
-    for (let j = 0; j < menu[i].ingredients.length; j++) {
-        sum += price[menu[i].ingredients[j]];
-    }
-    menu[i].priceCooking = sum;
+let sum = 0;
+for (let j = 0; j < menu[i].ingredients.length; j++) {
+    sum += price[menu[i].ingredients[j]];
+}
+menu[i].priceCooking = sum;
 };
-
 console.log(`total: ${JSON.stringify(menu)}`);
 
+//Посчитайте с помощью цикла профит для каждого блюда и запишите его в объект блюда. 
 
+result = '';
 for (let i = 0; i < menu.length; i++) {
-    menu[i].profit = menu[i].priceSale - menu[i].priceCooking;
-};
+      menu[i].profit = raznica(menu[i].priceSale, menu[i].priceCooking);
+      result += `Dish: ${menu[i].name} ; saleProfit: ${menu[i].profit}`};
+alert(result);
 
-console.log(`total2: ${JSON.stringify(menu)}`);
+  
 
+//   resultText += `Блюдо: ${meals[i].name} ; Прибыль от продажи: ${meals[i].profit}\n`;
+// };
 
-let allDescriptionMenu = '';
+// alert(resultText);
 
-for (let i = 0; i < menu.length; i++) {
-    let description = `Название блюда: ${menu[i].name}; Стоимость: ${menu[i].priceSale}\n`;
-    allDescriptionMenu += description;
-};
-console.log(`Меню:\n${allDescriptionMenu}`);
 
