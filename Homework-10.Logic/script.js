e6b2e9d18b2f5d9190664e8a489a747168597dd1
IuliiaBaldanova <yuliyatowa@mail.ru>
2023-03-15 22:34:15 -0400

    fix1503

---
+++ Homework-10.Logic/script.js
@@ -0,0 +1,20 @@
+
+let loginPass = {
+    'admin': 'root',
+    'user': '1234',
+    };
+
+let login = prompt ('Enter your name');
+let pass = prompt ('Enter your password');
+    if (login == 'admin' && pass == loginPass.admin || login == 'user' && pass == loginPass.user) {
+        alert ('You entered!');
+    }
+
+    else if (login !== 'admin' && login !== 'user') {
+        alert ('user undefined');
+    }
+    else if (pass !== loginPass.admin || pass !== loginPass.user){
+        alert ('uncorrect password');
+    };
+
+    
